440ba7babeff77ffad992df3a10c767f184e946e
Mike Percy <mpercy@cloudera.com>
2014-05-22 02:10:10 -0700

    Build both static & shared libraries of libcrcutil
    Use libtool for library generation.
    
    Also fix KUDU-249: Do not statically link to glibc.
    
    Statically linking to glibc and libstdc++ in the build of the example
    program "usage" causes an unnecessary build-time dependency on things
    like libc.a and libm.a, which don't ship by default in dev environments
    for some Linux distros (i.e. SUSE).

--- autogen.sh
+++ autogen.sh
@@ -52,15 +52,22 @@ echo $CXX_VERSION | grep -q "g++" && IS_GCC=1
 IS_CLANG=0
 echo $CXX_VERSION | grep -q "clang" && IS_CLANG=1
 
+if [ ! -d m4 ]; then
+  echo "Creating m4 directory"
+  mkdir m4
+fi
 
 echo "Generating preliminary configure.ac"
 autoscan
 
-sed 's/^AC_INIT(.*$/AC_INIT(crcutil, 1.0, crcutil@googlegroups.com)\
+sed -e 's/^AC_INIT(.*$/AC_INIT(crcutil, 1.0, crcutil@googlegroups.com)\
 AM_INIT_AUTOMAKE([foreign subdir-objects])\
-AC_PROG_RANLIB() \
+LT_INIT() \
 AC_CONFIG_FILES([Makefile]) \
-AC_OUTPUT()/' configure.scan >configure.ac
+AC_CONFIG_MACRO_DIR([m4]) \
+AC_OUTPUT()/' \
+-e 's/AC_PROG_RANLIB//' configure.scan >configure.ac
+
 
 # AC_OUTPUT(Makefile)/' configure.scan >configure.ac
 rm -f configure.scan
@@ -75,9 +82,10 @@ autoheader
 target=./Makefile.am
 echo "Generating ${target}"
 echo>${target} "AUTOMAKE_OPTIONS=foreign"
+echo>${target} "ACLOCAL_AMFLAGS=-I m4"
 
 # --pedantic -std=c99?
-crcutil_flags="-DCRCUTIL_USE_MM_CRC32=1 -Wall -msse2 -Icode -Iexamples -Itests -fPIC"
+crcutil_flags="-DCRCUTIL_USE_MM_CRC32=1 -Wall -msse2 -Icode -Iexamples -Itests"
 crcutil_flags="${crcutil_flags} -O3"
 if [[ "$IS_GCC" = "1" && "$(${CXX} -dumpversion)" > "4.4.9" ]]; then
   crcutil_flags="${crcutil_flags} -mcrc32"
@@ -86,32 +94,32 @@ elif [[ "$IS_CLANG" = "1" ]]; then
 fi
 
 echo>>${target} "AM_CXXFLAGS=${crcutil_flags}"
-if [ "$(uname -a | grep ^Darwin)" == "" ] && [[ "$(${CXX} -dumpversion)" > "4.4.9" ]]; then
-  # Static linking is not supported on Mac OS X.
-  # Use static linking on Linux, otherwise GCC 4.5.0 linker produces
-  # obscure warning (well, the code works but nevertheless).
-  echo>>${target} "AM_LDFLAGS=-static"
-fi
 echo>>${target} 'AM_CFLAGS=$(AM_CXXFLAGS)'
+
+# Build tests.
 echo>>${target} "check_PROGRAMS=crcutil_ut"
 echo>>${target} "TESTS=crcutil_ut"
 sources=$(ls tests/*.cc tests/*.c tests/*.h code/*.cc code/*.h | grep -v intrinsic | tr "\n" " ")
+echo>>${target} "crcutil_ut_CXXFLAGS = \$(AM_CXXFLAGS)" # get our own build work dir
 echo>>${target} "crcutil_ut_SOURCES=${sources}"
 
-# Don't install the crcutil "usage" example program.
+# Build, but don't install the crcutil "usage" example program.
 echo>>${target} "noinst_PROGRAMS=usage"
 echo>>${target} 'usage_CXXFLAGS=$(AM_CXXFLAGS) -Itests'
 sources=$(ls examples/*.cc examples/*.h code/*.cc code/*.h tests/aligned_alloc.h | grep -v intrinsic | tr "\n" " ")
 echo>>${target} "usage_SOURCES=${sources}"
 
-# Build a static library.
-echo>>${target} "lib_LIBRARIES=libcrcutil.a"
+# Build both a static and a dynamic library.
 libsources=$(ls examples/interface.cc examples/interface.h code/*.cc code/*.h tests/aligned_alloc.h | grep -v intrinsic | tr "\n" " ")
-echo>>${target} "libcrcutil_a_SOURCES=${libsources}"
+echo>>${target} "lib_LTLIBRARIES = libcrcutil.la"
+echo>>${target} "libcrcutil_la_CXXFLAGS = \$(AM_CXXFLAGS) -fPIC"
+echo>>${target} "libcrcutil_la_SOURCES = ${libsources}"
+echo>>${target} "libcrcutil_la_LDFLAGS = -version-info 0:0:0"
 echo>>${target} "crcutilhdrsdir=\$(includedir)/crcutil"
 echo>>${target} "crcutilhdrs_HEADERS=examples/interface.h"
 
 echo "Creating Makefile.in"
+libtoolize
 aclocal
 automake --add-missing
 autoconf
